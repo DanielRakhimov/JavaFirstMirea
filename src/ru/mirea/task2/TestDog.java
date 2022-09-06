62d643d6657e109a54f431162c5ae8c239d712d9
DanielRakhimov <777rdaniel@gmail.com>
2022-09-06 20:14:05 +0300

    task1*

--- src/ru/mirea/task2/TestDog.java
+++ src/ru/mirea/task2/TestDog.java
@@ -1,14 +1,17 @@
 package ru.mirea.task2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestDog {
 
     ArrayList<Dog> dogs = new ArrayList<>();
 
-    public void add(){
-
-
+    public void add(int n){
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            dogs.add(new Dog(scanner.nextInt(), scanner.next()));
+        }
 
     }
 
